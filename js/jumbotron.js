f0589c318cc4920daf0f4523d0a77caf04f013e1
Wei Zhou <mike.weizhou@gmail.com>
2016-03-02 18:18:39 -0800

    Changed indentation tabs -> spaces

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,147 +1,147 @@
 $(document).ready(function() {
 
-	/***********************************************
-	 * VARIABLES
-	 */
+    /***********************************************
+     * VARIABLES
+     */
 
-	// CAROUSEL SETTINGS
-	var timePerSlide= 5000;						// Time to show per slide in ms (1s = 1000ms)
-	var aniSpeed	= "fast";					// Animation speed in ms ("fast", "slow", 200, 500...)
+    // CAROUSEL SETTINGS
+    var timePerSlide= 5000;                     // Time to show per slide in ms (1s = 1000ms)
+    var aniSpeed    = "fast";                   // Animation speed in ms ("fast", "slow", 200, 500...)
 
-	// JQUERY OBJECTS
-	var slides 		= $("#jumbotron .row");		// Array of slides
-	var prevBut		= $("#slidePrevButton");	// Previous button
-	var nextBut		= $("#slideNextButton");	// Next button
+    // JQUERY OBJECTS
+    var slides      = $("#jumbotron .row");     // Array of slides
+    var prevBut     = $("#slidePrevButton");    // Previous button
+    var nextBut     = $("#slideNextButton");    // Next button
 
-	// SLIDER
-	var length 		= slides.length;			// Total # of slides
-	var current 	= 0;						// Current slide index
-	var locked		= false;					// Lockdown while slide animating
+    // SLIDER
+    var length      = slides.length;            // Total # of slides
+    var current     = 0;                        // Current slide index
+    var locked      = false;                    // Lockdown while slide animating
 
-	// TIMER
-	var timer;			// setInterval() instance
-	var startTime;		// Timer start time
-	var timeLeft;		// Time left for current slide
-
-
-	/***********************************************
-	 * INITIALIZATIONS
-	 */
-
-	// START TIMER
-	startTimer();
-
-	// MAKE PREV & NEXT BUTTONS VISIBLE
-	prevBut.css("display", "block");
-	nextBut.css("display", "block");
-
-	// PAUSE ON MOUSE OVER, RESUME ON MOUSE OUT
-	slides.hover(function() { pauseTimer(); }, function() { resumeTimer(); });
-	prevBut.hover(function() { pauseTimer(); }, function() { resumeTimer(); });
-	nextBut.hover(function() { pauseTimer(); }, function() { resumeTimer(); });
-
-	// PREV & NEXT BUTTONS
-	prevBut.click(function() { prevSlide(); restartTimer(); });
-	nextBut.click(function() { nextSlide(); restartTimer(); });
+    // TIMER
+    var timer;          // setInterval() instance
+    var startTime;      // Timer start time
+    var timeLeft;       // Time left for current slide
 
 
-	/***********************************************
-	 * TIMER
-	 */
+    /***********************************************
+     * INITIALIZATIONS
+     */
 
-	// PUBLIC FUNCTIONS
-	function restartTimer() {
-		clearTimer();
-		startTimer();
-	}
-	function startTimer() {
-		clearTimer();
-		setTimer(timePerSlide);
-	}
-	function pauseTimer() {
-		clearTimer();
-		updateTimeLeft();
-	}
-	function resumeTimer() {
-		clearTimer();
-		setTimer(timeLeft);
-	}
+    // START TIMER
+    startTimer();
 
-	// SET & CLEAR TIMEOUT
-	function setTimer(time) {
-		updateStartTime();
-		setTimeLeft(time);
+    // MAKE PREV & NEXT BUTTONS VISIBLE
+    prevBut.css("display", "block");
+    nextBut.css("display", "block");
 
-		timer = setTimeout(function() {
-			nextSlide();
-			startTimer();
-		}, time);
-	}
-	function clearTimer() {
-		clearTimeout(timer);
-	}
+    // PAUSE ON MOUSE OVER, RESUME ON MOUSE OUT
+    slides.hover(function() { pauseTimer(); }, function() { resumeTimer(); });
+    prevBut.hover(function() { pauseTimer(); }, function() { resumeTimer(); });
+    nextBut.hover(function() { pauseTimer(); }, function() { resumeTimer(); });
 
-	// MISC
-	function getTime() {
-		return new Date().getTime();
-	}
-	function setTimeLeft(time) {
-		timeLeft = time;
-	}
-	function updateTimeLeft() {
-		timeLeft -= getTime() - startTime;
-	}
-	function updateStartTime() {
-		startTime = getTime();
-	}
+    // PREV & NEXT BUTTONS
+    prevBut.click(function() { prevSlide(); restartTimer(); });
+    nextBut.click(function() { nextSlide(); restartTimer(); });
 
 
-	/***********************************************
-	 * SLIDER
-	 */
+    /***********************************************
+     * TIMER
+     */
 
-	// PUBLIC FUNCTIONS
-	function prevSlide() {
-	 	slideTo(getCurrentSlide(), getPrevSlide());
-	}
-	function nextSlide() {
-		slideTo(getCurrentSlide(), getNextSlide());
-	}
+    // PUBLIC FUNCTIONS
+    function restartTimer() {
+        clearTimer();
+        startTimer();
+    }
+    function startTimer() {
+        clearTimer();
+        setTimer(timePerSlide);
+    }
+    function pauseTimer() {
+        clearTimer();
+        updateTimeLeft();
+    }
+    function resumeTimer() {
+        clearTimer();
+        setTimer(timeLeft);
+    }
 
-	// ANIMATED SLIDING
-	function slideTo(hideThis, showThis) {
-		if (!locked) {
-			lock();
-			current = showThis;
+    // SET & CLEAR TIMEOUT
+    function setTimer(time) {
+        updateStartTime();
+        setTimeLeft(time);
 
-			getSlide(hideThis).fadeOut(aniSpeed, function() {
-				getSlide(showThis).fadeIn(aniSpeed, function() { unlock(); });
-			});
-		}
-	}
+        timer = setTimeout(function() {
+            nextSlide();
+            startTimer();
+        }, time);
+    }
+    function clearTimer() {
+        clearTimeout(timer);
+    }
 
-	// GET SLIDE (JQUERY OBJECT)
-	function getSlide(slide) {
-		return $(slides.get(slide));
-	}
+    // MISC
+    function getTime() {
+        return new Date().getTime();
+    }
+    function setTimeLeft(time) {
+        timeLeft = time;
+    }
+    function updateTimeLeft() {
+        timeLeft -= getTime() - startTime;
+    }
+    function updateStartTime() {
+        startTime = getTime();
+    }
 
-	// GET SLIDE INDEXES
-	function getCurrentSlide() {
-		return current;
-	}
-	function getPrevSlide() {
-		return (current - 1) < 0 ? length - 1 : current - 1;
-	}
-	function getNextSlide() {
-		return (current + 1) > (length - 1) ? 0 : current + 1;
-	}
 
-	// LOCK DOWN
-	function lock() {
-		locked = true;
-	}
-	function unlock() {
-		locked = false;
-	}
+    /***********************************************
+     * SLIDER
+     */
+
+    // PUBLIC FUNCTIONS
+    function prevSlide() {
+        slideTo(getCurrentSlide(), getPrevSlide());
+    }
+    function nextSlide() {
+        slideTo(getCurrentSlide(), getNextSlide());
+    }
+
+    // ANIMATED SLIDING
+    function slideTo(hideThis, showThis) {
+        if (!locked) {
+            lock();
+            current = showThis;
+
+            getSlide(hideThis).fadeOut(aniSpeed, function() {
+                getSlide(showThis).fadeIn(aniSpeed, function() { unlock(); });
+            });
+        }
+    }
+
+    // GET SLIDE (JQUERY OBJECT)
+    function getSlide(slide) {
+        return $(slides.get(slide));
+    }
+
+    // GET SLIDE INDEXES
+    function getCurrentSlide() {
+        return current;
+    }
+    function getPrevSlide() {
+        return (current - 1) < 0 ? length - 1 : current - 1;
+    }
+    function getNextSlide() {
+        return (current + 1) > (length - 1) ? 0 : current + 1;
+    }
+
+    // LOCK DOWN
+    function lock() {
+        locked = true;
+    }
+    function unlock() {
+        locked = false;
+    }
 
 });
