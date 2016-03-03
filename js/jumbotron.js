ffff0d41afcccd6c42c0d2ee7b85d34ef3a122d6
Wei Zhou <mike.weizhou@gmail.com>
2016-03-02 17:38:20 -0800

    Tidied up the code.

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
 	 */
 
 	// CAROUSEL SETTINGS
-	var timePerSlide= 5000;							// Time to show per slide in ms
-	var aniSpeed	= "fast";						// Animation speed in ms ("fast", "slow", 200, 500...)
+	var timePerSlide= 5000;						// Time to show per slide in ms (1s = 1000ms)
+	var aniSpeed	= "fast";					// Animation speed in ms ("fast", "slow", 200, 500...)
 
 	// JQUERY OBJECTS
-	var slides 		= $("#jumbotron .row");			// Array of slides
-	var prevBut		= $("#slidePrevButton");		// Previous button
-	var nextBut		= $("#slideNextButton");		// Next button
-	var slideTimer	= $("#slideTimer");				// Slide timer
+	var slides 		= $("#jumbotron .row");		// Array of slides
+	var prevBut		= $("#slidePrevButton");	// Previous button
+	var nextBut		= $("#slideNextButton");	// Next button
+	var slideTimer	= $("#slideTimer");			// Slide timer
 
 	// SLIDER
-	var length 		= slides.length;				// Total # of slides
-	var current 	= 0;							// Current slide index
-	var locked		= false;						// Lockdown while slide animating
+	var length 		= slides.length;			// Total # of slides
+	var current 	= 0;						// Current slide index
+	var locked		= false;					// Lockdown while slide animating
 
 	// TIMER
 	var timer;			// setInterval() instance
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
 
 	/***********************************************
-	 * EVENT HANDLERS
+	 * INITIALIZATIONS
 	 */
 
 	// START TIMER
@@ -132,7 +132,6 @@ $(document).ready(function() {
 	function getNextSlide() {
 		return (current + 1) > (length - 1) ? 0 : current + 1;
 	}
-
 
 	// LOCK DOWN
 	function lock() {
