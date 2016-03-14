cc1e154ee2db4e3501952d91a6661eb3d7b9c882
Joshua Abe <joshua.t.abe@gmail.com>
2016-03-14 00:19:01 -0700

    added universal header and changed everything to php

--- content/contentPS2.php
+++ content/contentPS2.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,63 +31,11 @@
 
   <!-- Navigation Bar
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-
-  <div id="header">
-    <div class="container">
-      <div class="row">
-        <div class="four columns">
-          <a href="../index.html" id="logo"><img src="../images/kappa.png" alt="SwimV Logo"></a>
-        </div>
-        <div class="eight columns">
-          <ul class="u-pull-right" id="headerbuttons">
-            <li><a class="button button-primary" id="button0" href="../swimkids.html">Swim Kids</a></li>
-            <li><a class="button button-primary" id="button1" href="../preschool.html">Preschool</a></li>
-            <li><a class="button button-primary" id="button2" href="../drills.html">Drills</a></li>
-            <li><a class="button button-primary" id="button3" href="../safety.html">Water Safety</a></li>
-            <li><a class="button button-primary" id="button4" href="../form.html">Questions</a></li>
-            <li><a class="button button-primary droplink" id="loginbutton" href="../login.html">Login</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="offset-by-five columns">
-        <div id="dropdown" class="dropContent seven columns">
-
-          <div id="leftCol">
-            <form method="post" action="http://webdevfoundations.net/scripts/formdemo.asp" onsubmit="return loginValidate()">
-              <h4>Login</h4>
-              <label for="username_login">Username</label>
-              <input class="u-full-width" id="username_login" type="text" name="username_login">
-              <label for="password_login">Password</label>
-              <input class="u-full-width" id="password_login" type="password" name="password_login">
-              <input class="button-primary u-pull-right" value="Submit" type="submit">
-            </form>
-          </div>
-          
-          <div id="rightCol" class="u-pull-right">
-            <form method="post" action="http://webdevfoundations.net/scripts/formdemo.asp" onsubmit="return signUpValidate()">
-              <h4>Register</h4>
-              <label for="username_register">Username</label>
-              <input class="u-full-width" id="username_register" type="text" name="username_register">
-              <label for="email">Email Address</label>
-              <input class="u-full-width" id="email" type="email" name="email_register">
-              <label for="password_register">Password</label>
-              <input class="u-full-width" id="password_register" type="password" name="password_register">
-              <label for="password_confirm">Confirm Password</label>
-              <input class="u-full-width" id="password_confirm" type="password" name="password_confirm_register">
-              <input class="button-primary u-pull-right" value="Register" type="submit">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
+<?php
+  include("header.php");
+?>
   <!-- Main Content
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<div id="content">
   <div class="container" id="level">
   	<h1>Duck</h1>
   </div>
@@ -139,8 +91,8 @@
   </div>
 
   <div class="container" id="pad">
-  	<a class="button" href="contentPS1.html">level 1</a>
-  	<a class="button u-pull-right" href="contentPS3.html">level 3</a>
+  	<a class="button" href="contentPS1.php">level 1</a>
+  	<a class="button u-pull-right" href="contentPS3.php">level 3</a>
   </div>
 </div>
 
@@ -158,7 +110,6 @@
 
   <!--Javascripts-->
   <script src="../js/jquery-2.2.0.js"></script>
-  <script type="text/javascript" src="../js/navbar.js"></script>
 
 </body>
 </html>
