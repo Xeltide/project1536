e4be17535d1ae89554427efc1c95898762073fc3
Wei Zhou <mike.weizhou@gmail.com>
2016-03-17 15:41:40 -0700

    Forgot to add head.php.

---
+++ shared/head.php
@@ -0,0 +1,23 @@
+<!-- Basic Page Needs
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicon
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="icon" type="image/png" href="<?php relativePath();?>images/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="<?php relativePath();?>images/favicon-16x16.png" sizes="16x16" />
+
+<!-- Mobile Specific Metas
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- FONT –––––––––––––––––––––––––––––––––––––––– -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+<!-- CSS
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="stylesheet" href="<?php relativePath();?>css/normalize.css">
+<link rel="stylesheet" href="<?php relativePath();?>css/skeleton.css">
+<link rel="stylesheet" href="<?php relativePath();?>css/base.css">
