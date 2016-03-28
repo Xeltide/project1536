e7605dcecd992d57055df0241b3d6273101f5f30
headhuntar <headhuntar@gmail.com>
2016-03-27 21:39:44 -0700

    update text on swim kids

--- swimkids.php
+++ swimkids.php
@@ -28,10 +28,14 @@
 		</div>
 		<div class="subItems">
 			<div class="five columns">
-				<p>Students will be introduced to a pool environment for the first time. Many instructors take their students on a tour around the pool area to point out dangers and meeting areas.</p>
-				<p>Students will need to identify when it is safe to enter the pool and choose the correct entry. Factors including depth of water, amount of swimmers and type of pool. Students will learn to choose from wade entry, ladder entry, slip-in and jump-ins.</p>
+				<p>Level 1 and 2 are meant for students with little to no previous experience. Students will be introduced to floats, glides and rhythmic breathing. If your child has previous swimming experience, you may visit your local swimming pool for a free swimming accessment.</p>
+				<ul>
+					<li>Class Size: 6 Swimmers</li>
+					<li>Distance 5 - 10 Meters</li>
+				</ul>
 			</div>
 			<div class="seven columns">
+					<p><strong>Safety</strong> - Students will need to identify when it is safe to enter the pool and choose the correct entry. Factors including depth of water, amount of swimmers and type of pool. Students will learn to choose from wade entry, ladder entry, slip-in and jump-ins.</p>
 					<p><strong>Flutter Kick</strong> - Kicking is essential to swimming. Students will learn to kick from their hips through to their toes with a flexed feet.</p>
 					<p><strong>Floats Front and Back</strong> - Floats will teach students to control the weight of their body. ears in water, slowly shift weight, spread wide to float. dont hold breath. understand the weight of different age groups.</p>
 					<p><strong>Glides Front and Back</strong> - </p>
@@ -50,9 +54,15 @@
 		</div>		
 		<div class="subItems">
 			<div class="five columns">
-				<p>Students will be introduced to sitting and kneeling dives. For the first time they will learn surface support.</p>
+				<p>In Level 3 and 4, students will build on the basics they've learnt and solidify the fundimentals. Students should be spending majority of time in the deep end.
+				</p>			
+				<ul>
+					<li>Class Size: 6 Swimmers</li>
+					<li>Distance 15 - 25 Meters</li>
+				</ul>
 			</div>
 			<div class="seven columns">
+					<p><strong>Safety</strong> - Students will be introduced to sitting and kneeling dives. For the first time they will learn surface support.</p>
 					<p><strong>Front Glide / Side Glide Combo</strong> - This is a very crucial skill. Allows swimmers to breath without (stopping/slwoing progress) of their front swim.</p>
 					<p><strong>Front Swim</strong> - Student will learn to recover their arms over the water. In combination with side glides students will be able to swim further and continuesly.</p>
 					<p><strong>Back Glide with Shoulder Rolls</strong> - learning the basics of rolling the body on a center axis. remember its the roll of the complete body and not just pushing of shoulers forward. shoulders should be opposite.</p>	
@@ -71,9 +81,14 @@
 		</div>	
 		<div class="subItems">
 			<div class="five columns">
-				<p>Students will learn standing dives. and self support in a stationary position of at least 1 minute.</p>
+				<p>sudents ill finally be swimming the length of the pool on a regular basis. they will learn the power strokes of the program, Front Crawl and BackCrawl. Many students have difficulty starting in level 5 as the endurance distances doubles from 25 meters to 50 meters and 3 new core skills are introduced.</p>
+				<ul>
+					<li>Class Size: 8 Swimmers</li>
+					<li>Distance: 50 Meters</li>
+				</ul>
 			</div>
 			<div class="seven columns">
+					<p><strong>Safety</strong> - Students will learn standing dives. and self support in a stationary position of at least 1 minute.</p>
 					<p><strong>Front Crawl</strong> - Students will build on their front swims here. High elbow recovery is introduced here. Make sure you have a good Front/side glide combo to have a great front swim. Some common drills are zipper, finger drag, and chicken swim.</p>
 					<p><strong>Back Crawl</strong> - Students be introduced to arm movements, stroke on their back. a good understanding of shoulderrolls is recommended. Make sure arms are completely straight. make sure strong kicks. full reach. no catch up. arms infront of and behind with shoulder rolls.</p>
 					<p><strong>Whip Kick</strong> - one of the hardest skills in swimming. students will be introduced to the resting stroke kick. key terms flexed feet, kneeds closer than ankles, body positioning.</p>
@@ -91,9 +106,14 @@
 		</div>	
 		<div class="subItems">
 			<div class="five columns">
-				<p>students will learn the dangres of ice, when are where tog o swimming and boating safety. Further more they will learn how to protect themselves and rescue others using a thorwing assist with a line.</p>
+				<p>Continue to develop their front and back crawls. Learn elementary back stroke and for the first time rescues.</p>
+				<ul>
+					<li>Class Size: 8 Swimmers</li>
+					<li>DIstance 75 - 150 Meters</li>
+				</ul>				
 			</div>
 			<div class="seven columns">
+					<p><strong>Safety</strong> - students will learn the dangres of ice, when are where tog o swimming and boating safety. Further more they will learn how to protect themselves and rescue others using a thorwing assist with a line.</p>
 					<p><strong>Front Crawl</strong> - Students will learn to breathe on their side without distruption to their movement. Breathing on side with no pause flawlessly. reaching infront of the heand and inline with shoulders. aka shoulder/body rolls and reach.</p>
 					<p><strong>Back Crawl</strong> - </p>
 					<p><strong>Elementary Back Crawl</strong> - adding the arms to whip kick, students will learn their first resting stroke. takes coordination and make sure to glide (have a resting phase).</p>
@@ -113,9 +133,14 @@
 		</div>	
 		<div class="subItems">
 			<div class="five columns">
-				<p>Students will be introduced to Rescue Breathing. In addition complications will be added to further students knowledge. Student will also learn ice safety. Students will learn more safety stuff...</p>
+				<p>Students will perfect their strokes and further develop endurance and strength.</p>
+				<ul>
+					<li>Class Size: 10 Swimmers</li>
+					<li>Distance: 300 - 500 Meters</li>
+				</ul>
 			</div>
 			<div class="seven columns">
+					<p><strong>Safety</strong> - Students will be introduced to Rescue Breathing. In addition complications will be added to further students knowledge. Student will also learn ice safety. Students will learn more safety stuff...</p>
 					<p><strong>Front Crawl</strong> - Student will learn bent arm pull and strengthen their strokes. stroke will be perfected. No pause breathing. Full reach, extensions and finish. Power phases and strong kicks. Etc.</p>
 					<p><strong>Back Crawl</strong> - students will learn bent arm pull / s pull. A stronger pulll to allow more power per stroke. A perfected reach and kick with correct body position.</p>
 					<p><strong>Elementary Backstroke</strong> - focus on good whip kick and glide check sheet for more info and put here.</p>
