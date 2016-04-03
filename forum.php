591ab752d7e8c6d65aa3ade161930e1356e1161b
dw331297 <donnsjuan@gmail.com>
2016-04-03 00:25:24 -0700

    minor style changes

--- forum.php
+++ forum.php
@@ -12,11 +12,11 @@
 	$result=mysql_query($sql);
 ?>
 
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+<table id="forumTable" width="100%" border="0" cellpadding="3" cellspacing="1">
 <tr>
-<td width="6%" bgcolor="#E6E6E6" class="centerData"><strong>#</strong></td>
-<td width="53%" bgcolor="#E6E6E6"><strong>Topic</strong></td>
-<td width="13%" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
+<td width="6%" bgcolor="#1E90FF" class="centerData"><strong>#</strong></td>
+<td width="53%" bgcolor="#1E90FF"><strong>Topic</strong></td>
+<td width="13%" bgcolor="#1E90FF"><strong>Date/Time</strong></td>
 </tr>
 
 <?php
@@ -35,6 +35,6 @@ while($rows=mysql_fetch_array($result)){ // Start looping table row
 mysql_close();
 ?>
 <tr>
-<td colspan="5" align="right" bgcolor="#E6E6E6" class="centerData"><a href="add_topic_form.php"><strong>Create New Topic</strong> </a></td>
+<td colspan="5" align="right" bgcolor="#1E90FF" class="centerData"><a href="add_topic_form.php"><strong>Create New Topic</strong> </a></td>
 </tr>
 </table>
