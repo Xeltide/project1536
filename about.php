8b45ccf4bf5280f20217d97933781ef9681382f4
headhuntar <headhuntar@gmail.com>
2016-04-03 23:21:10 -0700

    profile infos

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
       <div class="row">
         <div class="five columns" id="aboutSite">
           <h3>About</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex. Nunc rhoncus arcu odio, vitae dignissim nunc semper sit amet.</p>
+          <p>SwimVan is meant for sharing swimming information for everyone. Swimming is a vital life skills and everyone should have access and opportunity to learn more about it. Visit your local pool for seasonal brochures to check for lessons times or leave us a question and we'll be happy to help you.</p>
           <h5 class="overrideHColor">Get In Touch</h5>
           <a href="#"><img src="images/facebook.gif" alt=""></a>
           <a href="#"><img src="images/twitter.gif" alt=""></a>
@@ -41,34 +41,39 @@
               <td>
                 <img src="images/profile_pic_placeholder.jpg" alt="">
                 <h5 class="overrideHColor">Joshua Abe</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex.</p>
+                <h6>The Talent</h6>
+                <p>Talent young developer who wants to make unique and fresh indie games.</p>
               </td>
               <td>
                 <img src="images/profile_pic_placeholder.jpg" alt="">
                 <h5 class="overrideHColor">Stephen Cheng</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex.</p>
+                <h6>The ShotCaller</h6>
+                <p>Laid back but smart developer. Currently working on tool for optimizing player decisions for video games.</p>
               </td>
               <td>
                 <img src="images/profile_pic_placeholder.jpg" alt="">
                 <h5 class="overrideHColor">Allen Li</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex.</p>
+                <h6>The Average</h6>
+                <p>Developer whos balanced in all aspected. Interested in front end and back end for web development.</p>
               </td>
             </tr>
             <tr>
               <td>
                 <img src="images/profile_pic_placeholder.jpg" alt="">
                 <h5 class="overrideHColor">Donald Watson</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex.</p>
+                <h6>The Rocker</h6>
+                <p>Heavy metal and rock loving guy. Extremely hard working and has a ton of Grit.</p>
               </td>
               <td>
                 <img src="images/profile_pic_placeholder.jpg" alt="">
                 <h5 class="overrideHColor">Wei Zhou</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex.</p>
+                <h6>Mystery Man</h6>
+                <p>Mysterious developer who whos as a lone wolf. Able to bring past experiences from many fields.</p>
               </td>
               <td>
                 <img src="images/profile_pic_placeholder.jpg" alt="">
-                <h5 class="overrideHColor">Some Mascot</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut mi turpis. Nulla ac accumsan orci. Vivamus sed lectus dolor. Ut interdum imperdiet tortor in porta. Suspendisse semper interdum sagittis. Vestibulum et viverra ex.</p>
+                <h5 class="overrideHColor">SwimVan</h5>
+                <p>Our website is meant to share information and to educate general public on swimming..</p>
               </td>
             </tr>
           </table>
